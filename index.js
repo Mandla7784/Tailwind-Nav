351a0d1b31a61db65596b37e91ebed5c554db1cb
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-07-14 23:22:37 +0200

    Updated theme icon

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ const searchBar = document.querySelector(".search");
 const input = document.querySelector("input");
 // event listeners
 themeBtn.addEventListener("click", toggleTheme);
-
+let icon = themeBtn.querySelector("ion-icon");
 function toggleTheme() {
   // funtion to toggle theme when button is clicked
   navBar.classList.add("bg-[#151924]", "text-white"); // adds bg color and text color
   searchBar.classList.add("bg-[#2d313b]");
+  icon.setAttribute(
+    "name",
+    icon.getAttribute("name") === "moon-outline"
+      ? "sunny-outline"
+      : "moon-outline"
+  );
+  icon.classList.add("transition-all", "duration-500");
 
   input.classList.add("bg-[#2d313b]");
 }
