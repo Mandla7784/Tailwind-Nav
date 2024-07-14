2f8f8dfba221f617e6228fc5aada30b9cf753ef4
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-07-14 22:53:59 +0200

    Updated back color

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const themeBtn = document.querySelector("button");
 const navBar = document.querySelector("nav");
 const searchBar = document.querySelector(".search");
-
+const input = document.querySelector("input");
 // event listeners
 themeBtn.addEventListener("click", toggleTheme);
 
@@ -9,6 +9,6 @@ function toggleTheme() {
   // funtion to toggle theme when button is clicked
   navBar.classList.add("bg-[#151924]", "text-white"); // adds bg color and text color
   searchBar.classList.add("bg-[#2d313b]");
-  const input = document.querySelector("input");
+
   input.classList.add("bg-[#2d313b]");
 }
